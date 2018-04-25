b14fb7094adf844c8c726d205d183e201069393c
Jbin <kjb7907@naver.com>
2018-04-25 17:51:35 +0900

    commit

--- src/pages/page1.vue
+++ src/pages/page1.vue
@@ -96,20 +96,150 @@
     </v-layout>
 
 
-    <v-layout row wrap style="margin-top: 3px;">
-      <v-card color="grey lighten-4">
-        <div style="padding: 10px;">
-          <div style="background-color: red ; width:60px; height:3px; margin-left: 3px;"></div>
-          <h2>설비가동현황</h2>
-          <div style="overflow: scroll;">
-            <div style="width:200px; display:inline-block">asd</div>
-            <div style="width:4800px; display: inline-block;">asd</div>
-          </div>
-        </div>
-      </v-card>
+    <!--<v-layout row wrap style="margin-top: 3px;">-->
+      <!--<v-card color="grey lighten-4">-->
+        <!--<div style="padding: 10px;">-->
+          <!--<div style="background-color: red ; width:60px; height:3px; margin-left: 3px;"></div>-->
+          <!--<h2>설비가동현황</h2>-->
+          <!--<div style="overflow: scroll;">-->
+            <!--<div style="width:200px; display:inline-block">asd</div>-->
+            <!--<div style="width:4800px; display: inline-block;">asd</div>-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</v-card>-->
+    <!--</v-layout>-->
 
-    </v-layout>
+    <div class="section">
+      <h2 class="tit">설비가동현황</h2>
 
+
+      <div class="gantt" style="margin-top:0;"  id="operPrediction">
+
+
+        <div class="linearea">
+          <template>
+            <div class="line">
+              <div class="lineInfo">
+                <h2>펠렛1호</h2>
+                <p class="product">PB 원더플 마블</p>
+                <dl><dt>OEE</dt><dd>22%</dd></dl>
+                <dl><dt>시간가동률</dt><dd>22%</dd></dl>
+                <dl><dt>성능가동률</dt><dd>22%</dd></dl>
+                <dl><dt>양품률</dt><dd>22%</dd></dl>
+              </div>
+              <ul>
+                <li>계획</li>
+                <li>예상</li>
+                <li>가동</li>
+              </ul>
+            </div><!--/.line-->
+            <div class="line">
+              <div class="lineInfo">
+                <h2>펠렛1호</h2>
+                <p class="product">PB 원더플 마블</p>
+                <dl><dt>OEE</dt><dd>22%</dd></dl>
+                <dl><dt>시간가동률</dt><dd>22%</dd></dl>
+                <dl><dt>성능가동률</dt><dd>22%</dd></dl>
+                <dl><dt>양품률</dt><dd>22%</dd></dl>
+              </div>
+              <ul>
+                <li>계획</li>
+                <li>예상</li>
+                <li>가동</li>
+              </ul>
+            </div><!--/.line-->
+          </template>
+        </div><!--/.linearea-->
+
+        <div class="chartwrap" id="chartScroll">
+
+          <div class="chartarea" style="width:2525px;"><!-- width값 시간 * 40 -->
+
+            <ol class="day">
+              <li class="active"><p id="calcToday"></p>
+                <ol class="time">
+                  <li>07</li><li>08</li><li>09</li><li>10</li><li>11</li><li>12</li><li>13</li><li>14</li><li>15</li><li>16</li><li>17</li><li>18</li><li>19</li><li>20</li><li>21</li><li>22</li><li>23</li>
+                </ol>
+              </li>
+              <li class="active"><p id="calcTomorrow"></p>
+                <ol class="time">
+                  <li>00</li><li>01</li><li>02</li><li>03</li><li>04</li><li>05</li><li>06</li><li>07</li>
+                </ol>
+              </li>
+            </ol>
+
+            <template>
+              <div class="mchart">
+                <div class="currLine" style="display:none;border-left: 2.5px solid rgb(234, 71, 44);position: absolute;height: 135px;"></div>
+                <ul class="bg">
+                  <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+                  <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+                </ul>
+
+                <div class="plan">
+                  <p style="left:0px;width:200px;"><span>PB 캠펜 (15,000kg)</span><em> 22 ~ 22  <br> 22 (22kg) </em></p>
+                </div><!--/.plan-->
+
+                <div class="figure">
+                  <template>
+                    <p style="left:0px;width:200px;" class="fin"><span>asdd(22 kg)</span>
+                      <em>Production Date : 212<br>22 ~ 22<br> asd<br> Amount : 123 kg </em>
+                    </p>
+                    <template><span><i style="left:200px;"></i></span></template>
+                  </template>
+                </div><!--/.figure-->
+
+                <div class="present">
+                  <template>
+                    <p @click="getPop(run.runYn, run.procRunId)" style="left:0px;width:200px;cursor:pointer;" class="run"><b>asdd</b>
+                      <em>321 ~ 3213 (123)<br>123</em>
+                      <em>312 ~ 123 (123)<br>Operation of equipment</em>
+                    </p>
+                  </template>
+                </div><!--/.present-->
+
+              </div><!--/.mchart-->
+            </template>
+            <template>
+              <div class="mchart">
+                <div class="currLine" style="display:none;border-left: 2.5px solid rgb(234, 71, 44);position: absolute;height: 135px;"></div>
+                <ul class="bg">
+                  <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+                  <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+                </ul>
+
+                <div class="plan">
+                  <p style="left:0px;width:200px;"><span>PB 캠펜 (15,000kg)</span><em> 22 ~ 22  <br> 22 (22kg) </em></p>
+                </div><!--/.plan-->
+
+                <div class="figure">
+                  <template>
+                    <p style="left:0px;width:200px;" class="fin"><span>asdd(22 kg)</span>
+                      <em>Production Date : 212<br>22 ~ 22<br> asd<br> Amount : 123 kg </em>
+                    </p>
+                    <template><span><i style="left:200px;"></i></span></template>
+                  </template>
+                </div><!--/.figure-->
+
+                <div class="present">
+                  <template>
+                    <p @click="getPop(run.runYn, run.procRunId)" style="left:0px;width:200px;cursor:pointer;" class="run"><b>asdd</b>
+                      <em>321 ~ 3213 (123)<br>123</em>
+                      <em>312 ~ 123 (123)<br>Operation of equipment</em>
+                    </p>
+                  </template>
+                </div><!--/.present-->
+
+              </div><!--/.mchart-->
+            </template>
+
+          </div><!--/.chartarea-->
+
+        </div><!--/.chartwrap-->
+
+      </div><!--/.gantt-->
+
+    </div><!--/.section-->
 
 
 
